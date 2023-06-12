af96f37a404660de614f167f03135a22e604d5ed
Mayberry <2673220823@qq.com>
2023-06-12 23:05:17 +0800

    更新本地选择图片功能（部分）

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # drawingBoard
-这是一个简单的手机绘画应用，该应用可以保存图片以及完成一些基本的绘画功能。
+这是一个简单的手机绘画应用，该应用可以从本地选择图片作为背景，保存图片以及完成一些基本的绘画功能。
 
-This is a simple mobile drawing application. You can use this application as a drawing board and download the picture you have drawn.
+This is a simple mobile drawing application. You can use select a photo from your phone as the drawing background, use this application as a drawing board and download the picture you have drawn.
--- app/src/main/java/com/permissionx/drawingboard/MainActivity.kt
+++ app/src/main/java/com/permissionx/drawingboard/MainActivity.kt
@@ -2,12 +2,14 @@ package com.permissionx.drawingboard
 
 import android.Manifest
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.BounceInterpolator
@@ -18,6 +20,7 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private val fromAlbum = 2
     private var brushContainerFlag = false
     private var colorFlag = false
 
@@ -31,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         eraseBtnEvent()
         undoBtnEvent()
         saveBtnEvent()
+        pickBtnEvent()
     }
 
     private fun brushSizeEvent() {
@@ -132,7 +136,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun pickBtnEvent() {
+        pickBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "image/*"
+            startActivityForResult(intent, fromAlbum)
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            fromAlbum -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    data.data?.let { uri ->
+                        val bitmap = getBitmapFromUri(uri)
+                        bg.setImageBitmap(bitmap)
+                    }
+                }
+            }
+        }
+    }
 
+    private fun getBitmapFromUri(uri: Uri) = contentResolver.openFileDescriptor(uri, "r")?.use {
+        BitmapFactory.decodeFileDescriptor(it.fileDescriptor)
+    }
 
 }
